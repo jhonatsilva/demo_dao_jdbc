3a742b75c16614063c80ec657e901105cdd4debc
jhonatsilva <developer@jhonathansilva.com>
2023-07-04 20:39:27 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,17 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.Date;
 
-import com.mysql.cj.protocol.a.DebugBufferingPacketReader;
-
-import db.DB;
-import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "English Courses");
-		System.out.println(obj);
+		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob","bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 }
+
