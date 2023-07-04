399d0b406be64df722d003a931e56faf63120525
jhonatsilva <developer@jhonathansilva.com>
2023-07-03 21:58:41 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,35 +4,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import com.mysql.cj.protocol.a.DebugBufferingPacketReader;
+
 import db.DB;
 import db.DbIntegrityException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = null;
-		PreparedStatement st = null;
-		try {
-			conn = DB.getConnection();
-	
-			st = conn.prepareStatement(
-					"DELETE FROM seller "
-					+ "WHERE "
-					+ "Id = ?");
-
-			st.setInt(1, 6);
-			
-			int rowsAffected = st.executeUpdate();
-			
-			System.out.println("Done! Rows affected: " + rowsAffected);
-		}
-		catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
-		} 
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		Department obj = new Department(1, "English Courses");
+		System.out.println(obj);
 	}
 }
